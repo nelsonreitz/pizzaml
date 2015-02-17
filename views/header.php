8c9afe34b546d9f3bdae6714eda16e25d0450230
Nelson <nailsonne@gmail.com>
2015-02-17 16:11:03 -0500

    Header CSS

--- views/header.php
+++ views/header.php
@@ -12,6 +12,8 @@
         <title>Three Aces</title>
     <?php endif ?>
 
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+
     <link rel="stylesheet" href="../css/normalize.css">
     <link rel="stylesheet" href="../css/skeleton.css">
     <link rel="stylesheet" href="../css/style.css">
@@ -20,8 +22,10 @@
   <body>
 
     <header class="header" role="banner">
-      <h1 class="site-title"><a href="/">Three Aces</a></h1>
-      <a href="/cart.php">Shopping Cart</a>
+      <div class="container">
+        <h1 class="site-title"><a href="/">Three Aces</a></h1>
+        <a class="cartlink" href="/cart.php">Shopping Cart</a>
+      </div><!-- .container -->
     </header>
 
     <div class="container">
