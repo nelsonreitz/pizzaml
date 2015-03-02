264143e6036f6f403aba91942d2fcbd0dbbbb07b
Nelson <nailsonne@gmail.com>
2015-03-02 05:38:53 -0500

    Footer typo

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,6 @@
           <footer class="footer">
             <p>This is a fictive website built as an exercise for the <a href="http://cs75.tv">Computer Science E-75</a> course.</p>
-            <p>More info at <a href="https://github.com/nelsonreitz/project0">github/nelsonreitz/project0</a>.</p>
+            <p>More info at <a href="https://github.com/nelsonreitz/project0">github.com/nelsonreitz/project0</a>.</p>
             <p>&copy; Nelson Reitz - 2015</p>
           </footer>
         </div><!-- .content -->
