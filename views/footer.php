2d0cff901f644f7ecd5925a19c267a6ec2a3b775
Nelson Reitz <nailsonne@gmail.com>
2016-05-09 16:09:43 +0200

    Update copyright year

--- views/footer.php
+++ views/footer.php
@@ -1,7 +1,7 @@
           <footer class="footer">
             <p>This is a fictive website built as an exercise for the <a href="http://cs75.tv">Computer Science E-75</a> course.</p>
             <p>More info at <a href="https://github.com/nelsonreitz/project0">github.com/nelsonreitz/project0</a>.</p>
-            <p>&copy; Nelson Reitz - 2015</p>
+            <p>&copy; Nelson Reitz - 2016</p>
           </footer>
         </div><!-- .content -->
       </div><!-- .row -->
