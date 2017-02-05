43161224a2ea99f01f9b83cd61de6831e50a8bb6
Nelson Reitz <nailsonne@gmail.com>
2017-02-05 16:58:31 +0100

    Featured item styling update

--- views/index.php
+++ views/index.php
@@ -3,10 +3,40 @@
   <a href="#"><img class="featured-item-photo" src="img/featured_item_photo.png" alt="Featured Item photo"></a>
 
   <div class="item">
+
     <div class="item-infos">
-      <h2 class="item-name"><a href="#">Featured Item</a></h2>
+      <h2 class="item-name">Featured Item</h2>
       <p class="item-description">Featured Item description</p>
+
+      <form class="order-form" action="#" method="">
+
+      	<ul class="item-prices">
+
+    	  <li class="item-price">
+    	  	<input id="Featureditemsmallradio" type="radio" name="size" value="small">
+    	  	<label for="Featureditemsmallradio">
+    	  	  small: <span class="sizeprice">$9.90</span>
+    	  	</label>
+    	  </li>
+
+    	  <li class="item-price">
+    	  	<input id="Featureditemlargeradio" type="radio" name="size" value="large" checked>
+    	  	<label for="Featureditemlargeradio">
+    	  	  large: <span class="sizeprice">$14.90</span>
+    	  	</label>
+    	  </li>
+
+      	</ul>
+
+      	<div class="order-submit">
+	      <input class="order-quantity" type="number" name="quantity" value="1" min="1" max="<?= MAX_QUANT ?>" />
+	      <input class="primary button" type="submit" value="Order">
+      	</div><!-- .order-submit -->
+
+      </form>
+
     </div><!-- .item-infos -->
+    
   </div><!-- .item -->
 
 </div><!-- .featured-item -->
