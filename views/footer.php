b80022b58a26a8b8ea05bda3b198536adafe2ac7
Nelson Reitz <nailsonne@gmail.com>
2017-02-20 15:01:01 +0100

    Update Footer github link

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,6 @@
           <footer class="footer">
             <p>This is a fictive website built as an exercise for the <a href="http://cs75.tv">Computer Science E-75</a> course.</p>
-            <p>More info at <a href="https://github.com/nelsonreitz/pizzaml">github.com/nelsonreitz/project0</a>.</p>
+            <p>More info at <a href="https://github.com/nelsonreitz/pizzaml">github.com/nelsonreitz/pizzaml</a>.</p>
             <p>&copy; Nelson Reitz - <?= date("Y") ?></p>
           </footer>
         </div><!-- .content -->
